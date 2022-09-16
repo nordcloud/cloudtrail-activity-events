34a0b7036ea1fa95d527c81dba0bb068af331166
kamil <kamil.piotrowski@nordcloud.com>
2022-09-16 08:55:19 +0200

    Add example script

--- examples/send-test-event.sh
+++ examples/send-test-event.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-export AWS_REGION=us-east-1
-export AWS_DEFAULT_REGION=us-east-1
-
 # CREATING EVENT DATA STORE
 # aws cloudtrail create-event-data-store \
 #     --name test \
+#     --region us-east-1 \
 #     --retention-period 30 \
 #     --advanced-event-selectors '[{
 #         "Name": "Select all external events",
@@ -21,16 +19,16 @@ export AWS_DEFAULT_REGION=us-east-1
 #  --source "Custom" \
 #  --destinations '[{
 #         "Type": "EventDataStore", 
-#         "Location": "95bcb1bd-787f-4cc2-a7a3-1a8b721123c0"
+#         "Location": "EXAMPLE-787f-4cc2-a7a3-EXAMPLE"
 #     }]'
 
-export CHANNEL_ARN="arn:aws:cloudtrail:us-east-1:313457394549:channel/6464f5be-705b-4c51-b64c-d46643ff96dc"
-export ACCOUNT_ID="313457394549"
+export CHANNEL_ARN="arn:aws:cloudtrail:us-east-1:EXAMPLE:channel/EXAMPLE-fb04-4ffa-8170-EXAMPLE"
+export ACCOUNT_ID="EXAMPLE"
 export UUID="47f43c67-e673-4f29-a637-45c1cd9d77cf"
 
 aws cloudtrail-data put-audit-events \
     --channel-arn $CHANNEL_ARN \
     --audit-events '{
         "id": "6df0f951-6f16-4e2b-adb4-ababba1edc53",
-        "eventData": "{\"eventTime\":\"2022-09-07T12:00:10Z\",\"version\":\"0.01\",\"UID\":\"'$UUID'\",\"recipientAccountId\":\"'$ACCOUNT_ID'\",\"userIdentity\":{\"type\":\"USER\",\"principalId\":\"name@example.com\"},\"eventSource\":\"com.nordcloudapp.com.klarity\", \"eventName\":\"discoveryRule.created\",\"requestParameters\":{\"ruleName\":\"rule name\",\"ruleId\":\"00000000-00000000-00000000-00000001\"}}"
+        "eventData": "{\"eventTime\":\"2022-09-07T12:00:10Z\",\"version\":\"0.01\",\"UID\":\"'$UUID'\",\"recipientAccountId\":\"'$ACCOUNT_ID'\",\"userIdentity\":{\"type\":\"USER\",\"principalId\":\"name@example.com\"},\"eventSource\":\"com.nordcloudapp.autobackups\", \"eventName\":\"discoveryRule.created\",\"requestParameters\":{\"ruleName\":\"rule name\",\"ruleId\":\"00000000-00000000-00000000-00000001\"}}"
     }'
